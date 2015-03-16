0fafac645607e6c36d6d07bded6850af20e5316c
willy2706 <willylie2706@gmail.com>
2015-03-16 18:40:32 +0700

    cobain gabung dengan angular

---
+++ app/js/app.js
@@ -0,0 +1,20 @@
+var phonecatApp = angular.module('phonecatApp', [
+  'ngRoute',
+  'phonecatControllers'
+]);
+
+phonecatApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: 'app/partials/phone-list.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'app/partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/phones'
+      });
+  }]);
---
+++ app/js/controller.js
@@ -0,0 +1,15 @@
+var phonecatControllers = angular.module('phonecatControllers', []);
+
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    $http.get('phone.json').success(function(data) {
+      $scope.phones = data;
+    });
+
+    $scope.orderProp = 'id';
+  }]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+    $scope.phoneId = $routeParams.phoneId;
+  }]);
