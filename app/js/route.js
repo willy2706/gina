c6b46e7de2f37814beb9670ddb1dc28c5b8cfe1d
joshuabezaleel <joshua.bezaleel@gmail.com>
2015-04-06 19:21:06 +0700

    datakependudukan.html

--- app/js/route.js
+++ app/js/route.js
@@ -43,7 +43,7 @@ ginaRoutes.config(['$stateProvider', '$urlRouterProvider',
       })
       .state('contact', {
         url: '/contact',
-        templateUrl: 'app/partials/contact.html',
+        templateUrl: 'app/partials/datakependudukan.html',
         controller: ''
       })
   }]);
